ff1ef2e683586251175f1a87e3961519838fa00a
Your Name <mickelvalentine@hotmail.com>
2024-03-17 06:38:52 -0400

    added middleware

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require("cors")
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
+
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'sample_mflix',
@@ -14,3 +16,14 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+    //PORT = 8000
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`server is running on port`)
+})
