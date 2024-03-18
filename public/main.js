d01c07c25d9569cc6bf6c71ac747c956f948e322
Your Name <mickelvalentine@hotmail.com>
2024-03-18 05:28:45 -0400

    added fully functional copy of Port, DB, and middleware config

---
+++ public/main.js
@@ -0,0 +1,2 @@
+//this is where u put the code to reach out to the server to send info back n forth.
+//info can travel back n forth using fetches.
--- server.js
+++ server.js
@@ -1,29 +1,46 @@
-const express = require ('express')
-const app = express()
-const cors = require("cors")
-const MongoClient = require('mongodb').MongoClient
-require('dotenv').config()
-const PORT = 8000
+//Declare variables
+require('dotenv').config();
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require("cors");
+const app = express();
 
-let db,
-    dbConnectionString = process.env.DB_STRING,
-    dbName = 'sample_mflix',
-    collection
+//connect to port number from .env file
+const PORT= process.env.PORT || 4000
 
-MongoClient.connect(dbConnectionString)
-    .then(client =>{
-        console.log('connected to the database')
-        db = client.db(dbName)
-        collection = db.collection('movies')
-    })
+//connect to mongodb
+mongoose
+.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.g0khgvc.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`)
+.then(()=> console.log('MONGODB Connected'))
+.catch(err => console.log(err))
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+//set middleware
+
+//enable cors 
 app.use(cors())
 
-    //PORT = 8000
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`server is running on port`)
+//server static files
+app.use(express.static('public'))
+
+
+//Parse request
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+//set ejs as template
+app.set('view engine', 'ejs')
+
+//logic goes here
+app.listen(PORT, () => {
+    console.log("server is runing, catch it")
 })
+
+
+
+
+
+
+
+ 
+
